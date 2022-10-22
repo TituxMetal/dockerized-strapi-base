dcd4ffd83f6f682b110478121a1e18d00d3b73a9
Titux Metal (DEV) <github@lgdweb.fr>
2022-10-22 19:09:43 +0200

    Updating middlewares.js and plugins.js files in config; adds cloudinary upload provider configuration

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -15,5 +15,24 @@ module.exports = ({ env }) => ({
         defaultReplyTo: env('SMTP_TO')
       }
     }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET')
+      },
+      actionOptions: {
+        upload: {
+          folder: env('CLOUDINARY_FOLDER', 'strapi')
+        },
+        uploadStream: {
+          folder: env('CLOUDINARY_FOLDER', 'strapi')
+        },
+        delete: {}
+      }
+    }
   }
 })
